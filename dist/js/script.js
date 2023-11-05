a86799088bd66aa627ce737094e4087d89c1160d
FnsAja <michaelalfonso95@gmail.com>
2023-11-05 22:07:05 +0700

    Update Portfolio Tailwind CSS

---
+++ dist/js/script.js
@@ -0,0 +1,20 @@
+//Script Button Hamburger
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+//Script Navbar Fixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
